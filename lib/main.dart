dfd83d1814b270960e05ab71b30587fcd2b999b9
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-06 10:51:47 -0600

    incio de app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
+
+void main() => runApp(const MapsApp());
+
+class MapsApp extends StatelessWidget {
+  const MapsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home:LoadingScreen()
+      );
+    
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,6 @@
+export  'package:app_mapas/screens/map_screen.dart';
+export  'package:app_mapas/screens/gps_access_screen.dart';
+export  'package:app_mapas/screens/loading_screen.dart';
+
+
+
