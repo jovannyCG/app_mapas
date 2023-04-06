82e834750861dd22a8e6c589b303b170eda7fe82
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-06 12:18:04 -0600

    configuracion de geolocator

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -12,5 +13,21 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
     on<GpsAndPermissionEvent>((event, emit) => emit(state.copyWith(
         isGpsEnable: event.isGpsEnable,
         isGpsPermissionGranted: event.isGpsPermissionGranted)));
+        _init();
   }
+
+Future<void> _init()async {
+final isEnable = await _checkGpsStatus();
+
+}
+Future<bool> _checkGpsStatus()async{
+
+final isEnable = await Geolocator.isLocationServiceEnabled();
+Geolocator.getServiceStatusStream().listen((event) { 
+final isEnable =(event.index == 1) ? true: false;
+
+});
+  return isEnable;
+}
+
 }
