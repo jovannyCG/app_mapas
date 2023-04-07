b72b56f6dec5943e7e38c78f7c3f893067208542
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-07 13:39:53 -0600

    seguir la ubicacion del usuario

--- lib/blocs/location/location_bloc.dart
+++ lib/blocs/location/location_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'location_event.dart';
 part 'location_state.dart';
@@ -9,4 +10,16 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
     on<LocationEvent>((event, emit) {
     });
   }
+
+  Future getCurrentPosition()async{
+final position =await Geolocator.getCurrentPosition();
+print(position);
+  }
+
+  Future startFollowingUser()async{
+    Geolocator.getPositionStream().listen((event) {  
+      final position = event;
+      print(position);});
+   
+}
 }
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,10 +1,24 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/blocs.dart';
 import 'package:flutter/material.dart';
 
 
-class MapsScreen extends StatelessWidget {
+class MapsScreen extends StatefulWidget {
   const MapsScreen({super.key});
 
+  @override
+  State<MapsScreen> createState() => _MapsScreenState();
+}
 
+class _MapsScreenState extends State<MapsScreen> {
+  @override
+  void initState() {
+  final locationBloc = BlocProvider.of<LocationBloc>(context);
+  locationBloc.startFollowingUser();
+  //locationBloc.getCurrentPosition();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
