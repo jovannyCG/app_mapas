ff7c37cea2b1f4d4393bef30464409064e08f715
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-13 19:16:35 -0600

    decodificar geometry

--- lib/blocs/search/search_bloc.dart
+++ lib/blocs/search/search_bloc.dart
@@ -1,22 +1,32 @@
+import 'package:app_mapas/models/models.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:app_mapas/services/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
 part 'search_event.dart';
 part 'search_state.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
+  TrafficService trafficService;
 
-TrafficService trafficService;
-
-  SearchBloc({
-    required this.trafficService
-  }) : super(const SearchState()) {
-    on<OnActivateManualMarkerEvent>((event, emit) => emit(state.copyWhith(displayManualMarker: true)));
-  on<OnDesActivateManualMarkerEvent>((event, emit) => emit(state.copyWhith(displayManualMarker: false)));
+  SearchBloc({required this.trafficService}) : super(const SearchState()) {
+    on<OnActivateManualMarkerEvent>(
+        (event, emit) => emit(state.copyWhith(displayManualMarker: true)));
+    on<OnDesActivateManualMarkerEvent>(
+        (event, emit) => emit(state.copyWhith(displayManualMarker: false)));
   }
-  Future getCoorsStartToEnd( LatLng start, LatLng end ) async{
-    final resp = await trafficService.getCoorsStartToEnd(start, end);
-    return resp;
+  Future<RouteDestination> getCoorsStartToEnd(LatLng start, LatLng end) async {
+    final trafficResponse = await trafficService.getCoorsStartToEnd(start, end);
+    final geometry = trafficResponse.routes[0].geometry;
+    final distance = trafficResponse.routes[0].distance;
+    final duration = trafficResponse.routes[0].duration;
+
+    final points = decodePolyline(geometry, accuracyExponent: 6);
+    final latLngList = points
+        .map((coor) => LatLng(coor[0].toDouble(), coor[1].toDouble()))
+        .toList();
+    return RouteDestination(
+        points: latLngList, duration: duration, distance: distance);
   }
 }
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,4 @@
 
 export 'package:app_mapas/models/traffic_response.dart';
 export 'package:app_mapas/models/search_result.dart';
+export 'package:app_mapas/models/route_destination.dart';
