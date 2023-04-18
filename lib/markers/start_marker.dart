5bec36b64e3d8dd318543179d4888d76ed828d11
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-18 11:57:53 -0600

    continuación de diseño de marcador

--- lib/markers/start_marker.dart
+++ lib/markers/start_marker.dart
@@ -3,22 +3,34 @@ import 'package:flutter/material.dart';
 class StarTMarkerPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    final blackpaint = Paint()..color =Colors.black;
-
-    const double circleBlackRadius=20;
+    final blackpaint = Paint()..color = Colors.black;
+    final whitepaint = Paint()..color = Colors.white;
+    const double circleBlackRadius = 20;
+    const double circleWhiteRadius = 7;
     canvas.drawCircle(
-     Offset(circleBlackRadius, size.height-circleBlackRadius), 
-      circleBlackRadius, 
-      blackpaint);
+        Offset(circleBlackRadius, size.height - circleBlackRadius),
+        circleBlackRadius,
+        blackpaint);
+    canvas.drawCircle(
+        Offset(circleBlackRadius, size.height - circleBlackRadius),
+        circleWhiteRadius,
+        whitepaint);
 
+    final path = Path();
+    path.moveTo(40, 20);
+    path.lineTo(size.width - 10, 20);
+    path.lineTo(size.width - 10, 100);
+    path.lineTo(40, 100);
+    canvas.drawShadow(path, Colors.black, 10, false);
+    canvas.drawPath(path, whitepaint);
+
+   const blacBox = Rect.fromLTWH(40, 20, 70, 80);
+   canvas.drawRect( blacBox, blackpaint);
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) =>true;
-    
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 
   @override
-  bool shouldRebuildSemantics(covariant CustomPainter oldDelegate) =>false;
-  
-
+  bool shouldRebuildSemantics(covariant CustomPainter oldDelegate) => false;
 }
